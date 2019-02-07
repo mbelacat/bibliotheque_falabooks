e0a6bc2b77b2a6042df4bc542a3342cead4b5c81
Fatmoubidou <fatma.elammari@gmail.com>
2019-02-07 12:48:16 +0100

    user controller and templating all and single

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
@@ -21,8 +22,40 @@ class UserController extends AbstractController
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        // look for *all* User objects
+        $users = $repository->findAll();
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'controller_name' => 'UserController',"users"=> $users
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user", requirements={"id"="\d+"})
+     */
+    public function single($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        // look for User objects by Id
+        $user = $repository->find($id);
+        return $this->render('user/single.html.twig', ["user"=> $user
+        ]);
+    }
+
+    /**
+     * @Route("/users/new", name="user_new")
+     */
+    public function new()
+    {
+        $user = new User();
+        $user->setLastname('Mbemba')
+        ->setFirstname('Mbela')
+        ->setEmail('mbela.mbemba@gmail.com')
+        ->setLogin('mbela');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return $this->render('user/index.html.twig', ["users"=> $user
         ]);
     }
 }
