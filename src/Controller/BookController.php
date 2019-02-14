7ac7d9889ef4a2136cd42d9ecf6a039ef3e1159a
mbela mbemba <mbela.mbemba@gmail.com>
2019-02-14 22:56:14 +0100

    pagination for books in index ok

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -10,11 +10,13 @@ use App\Form\SortByBookCategoryType;
 use App\Form\SearchBookType;
 
 use App\Repository\BookRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Knp\Component\Pager\PaginatorInterface;
+use Knp\Bundle\PaginatorBundle\Definition\PaginatorAware;
 
 /**
  * Require ROLE_ADMIN for *every* controller method in this class.
@@ -22,13 +24,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  * @IsGranted("ROLE_ADMIN")
  * @Route("/books")
  */
-class BookController extends AbstractController
+class BookController extends Controller
 {
     /**
      * @Route("/", name="book_index", methods={"GET", "POST"})
      */
 
-    public function index(BookRepository $bookRepository, Request $request): Response
+    public function index(PaginatorInterface $paginator, BookRepository $bookRepository, Request $request): Response
     {
         $formBorrow = $this->createForm(EmpruntType::class); //creation du formulaire d'action d'emprunt
         $form = $this->createForm(SortByBookCategoryType::class);
@@ -38,8 +40,20 @@ class BookController extends AbstractController
              $category = $form->getData()["name"];
           }
           $books = $this->getDoctrine()->getRepository(Book::class)->findByCategory($this->getUser()->getLibrary(), $category );
+          /* @var $paginator \Knp\Component\Pager\Paginator */
+          $paginator  = $this->get('knp_paginator');
+
+          // Paginate the results of the query
+        $paginatedBooks = $paginator->paginate(
+            // Doctrine Query, not results
+            $books,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            5
+          );
         return $this->render('book/index.html.twig', [
-            'books' => $books,
+            'books' => $paginatedBooks,
             "current_menu" => "pret",
             'form' => $form->createView(),
             'formBorrow' => $formBorrow->createView(),
--- src/Kernel.php
+++ src/Kernel.php
@@ -17,11 +17,7 @@ class Kernel extends BaseKernel
 
     public function registerBundles()
     {
-        return array(
-          // ...
-          new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-          // ...
-        );
+        
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
